32d89e51909ff7080cb6c5ff27b3f750814ff0f0
Coolpower <martijnvansluijs@gmail.com>
2016-02-05 10:47:42 +0100

    Add and index for images

--- src/index.php
+++ src/index.php
@@ -2,5 +2,5 @@
 include 'resources/includes/PageCreator.php';
 $page = new PageCreator;
 $page->title = "Check je stress";
-$page->body = "<p>Hello world!<p>";
+$page->body = "<p>Hello world!<p><br><a href="resources/img/img_index.html">Index for images</a>";
 $page->create();
