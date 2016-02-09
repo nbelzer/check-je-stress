1747f51c7cd192e6ce33383bb7ee59463cb47847
Nick Belzer <nickbelzer@gmail.com>
2016-02-09 20:49:29 +0100

    Made a basic version of the index page

--- src/index.php
+++ src/index.php
@@ -2,6 +2,46 @@
 include 'resources/includes/PageCreator.php';
 $page = new PageCreator;
 $page->title = "Check je stress";
-$page->body = "<p>Hello world!<p><br><a href=\"resources/img/img_index.html\">Index for images</a>";
+$page->head = "<link rel=\"stylesheet\" href=" .$path_to_root . "resources/css/specific/index.css type=\"text/css\">";
+$page->body = <<<CONTENT
+
+<div class="row">
+  <div class="indexImage">
+    <ul class="title">
+      <li><img src="/resources/img/logo_highres.png" class="logo"></li>
+      <li><div class="logoTitle">Check Je Stress</div></li>
+    </ul>
+  </div>
+</div>
+
+<div class="row">
+  <a href="#">
+    <div class="small-12 medium-3 columns indexElement indexButton">
+      Doe een test
+    </div>
+  </a>
+  <a href="#">
+    <div class="small-12 medium-3 columns indexElement indexButton">
+      Informatie
+    </div>
+  </a>
+  <a href="#">
+    <div class="small-12 medium-3 columns indexElement indexButton">
+      Contact
+    </div>
+  </a>
+  <a href="#">
+    <div class="small-12 medium-3 columns indexElement indexButton">
+      Contact
+    </div>
+  </a>
+</div>
+
+<div class="row indexElement">
+
+  Meer informatie hierzo
+</div>
+
+CONTENT;
 $page->includeMenu = false;
 $page->create();
