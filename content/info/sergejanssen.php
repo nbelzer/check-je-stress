64b7c349368e0f3ad907f49d5f328de3570eaa6c
Omniscimus <omniscimus@icloud.com>
2016-02-16 22:38:03 +0100

    Put final original texts in content/ (#13)
    De pagina's in content/info/ kunnen evt. (deels) samengevoegd worden,
    ligt een beetje aan de opmaak.

---
+++ content/info/sergejanssen.php
@@ -0,0 +1,27 @@
+<h1>Serge Janssen</h1>
+
+<!-- Foto (inline plz lol) -->
+
+<p>
+  Mijn naam is Serge Janssen.
+</p>
+
+<p>
+  Sinds 1995 sta ik met veel plezier voor de klas omdat ik met liefde en plezier dit vak uitvoer. Toch kunnen door omstandigheden je ervaringen en gevoelens rondom het geen je doet veranderen. Zelf heb ik dit aan den lijve ondervonden toen ik een aantal jaar geleden verzeild raakte in een arbeidsconflict bij een werkgever. 
+  <br />
+	Heel geleidelijk veranderde ik van een bevlogen actieve docent in een doodvermoeide, cynische doemdenker; hard op weg naar een burn-out. De leerlingen hebben hier nooit iets van meegekregen, maar het thuisfront en vrienden in mijn directe omgeving hadden het veel eerder in de gaten dan ikzelf.
+</p>
+
+<p>
+  Gelukkig had ik de mogelijkheid om van baan te veranderen en dat heeft me gered van een burn-out. Ervaringsdeskundig wil ik me niet noemen, maar deze ervaring heeft me doen besluiten een vierjarige studie te volgen als Specialistisch Counsellor met stress & burn-out als speerpunt. Met het oprichten van het Steunpunt Stress Burnout Nederland (SSBN) wil ik bedrijven en particulieren ondersteunen om burn-out te voorkomen en te begeleiden naar een duurzaam werkleven. Tenslotte beslaat werk het grootste deel van de week en ons leven. Dan kan je het maar beter goed naar je zin hebben!
+</p>
+
+<p>
+  Met beleidsplannen schrijven ter preventie van burnout voor bedrijven en 1 op 1 gesprekken voeren met betrokkenen, geeft me het gevoel iets te kunnen betekenen!
+  <br />
+	Misschien ook wel voor jou of voor iemand in jouw omgeving! Vul het <a href="../contact/">contactformulier</a> in of bel me persoonlijk: 06 - 38 55 31 95
+</p>
+
+<p>
+	Tot binnenkort?
+</p>
