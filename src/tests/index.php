2f3d0b1eb8aec83b4a3b94488e02a9055fcb8695
Omniscimus <omniscimus@icloud.com>
2016-02-17 13:30:23 +0100

    Add test index page

--- src/tests/index.php
+++ src/tests/index.php
@@ -1,4 +1,21 @@
-A: Quick-test
-B: test voor managers, bestuurders en directeuren
-C: uitgebreide test
-D: burn-outrisicoanalyse voor bedrijven, ondernemingen en organisaties
+<?php
+include 'resources/includes/PageCreator.php';
+$page = new PageCreator;
+$page->title = "Tests";
+$page->body = <<<CONTENT
+
+<a href="www.checkjestress.nl/testen.php">Klik op deze link voor alle testen.</a>
+
+<ul>
+  <li><a href="testa/">Test A</a> is een Quick-test (25 items duur: ± 2 minuten)</li>
+  <li><a href="testb/">Test B</a> is een test voor managers, bestuurders en directeuren (25 items duur ± 5 minuten)</li>
+  <li><a href="testc/">Test C</a> is een uitgebreide test (56 items duur ± 15 minuten)</li>
+  <li><a href="testd/">Test D</a> is een deel van de burn-outrisicoanalyse voor bedrijven, ondernemingen en organisaties. (56 items duur ± 15 minuten)</li>
+</ul>
+<p>
+  Alle feedback verschijnt na het versturen op je scherm en je weet meteen het resultaat. Uiteraard worden alle verstuurde gegevens vertrouwelijk door ons behandeld. 
+</p>
+
+CONTENT;
+$page->includeMenu = true;
+$page->create();
