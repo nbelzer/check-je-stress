ecc49a15d47f34a1e37f5a90bb82cd0a1dea80a5
Omniscimus <omniscimus@icloud.com>
2016-02-20 13:46:24 +0100

    Improve sliders in test A (#5)

--- src/tests/testa.php
+++ src/tests/testa.php
@@ -2,15 +2,58 @@
 require_once '../resources/includes/PageCreator.php';
 $page = new PageCreator;
 $page->path_to_root = '../';
-$page->title = "Quicktest";
+$page->title = "Quicktest - CheckJeStress";
 $page->includeMenu = true;
+$page->head = <<<EOF
+<script src="resources/js/vendor/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<script>
+  window.onload = function() {
+    // Zoek alle slider divs op en maak de sliders.
+    var sliders = document.getElementsByClassName("slider_handle");
+    for (var i = 0; i < sliders.length; i++) {
+      $(sliders[i]).slider({
+        // Van 0 tot 100 zodat het sliden smooth gaat en we de slider in het
+        // midden kunnen zetten wanneer de pagina laadt.
+        min: 0,
+        max: 100,
+        // Initial value is 50
+        value: 50,
+        // Deze functie wordt aangeroepen zodra de gebruiker de slider loslaat.
+        // Zorgt ervoor dat er maar 6 mogelijkheden zijn.
+        stop: function(event, ui) {
+          var newValue;
+          if (ui.value < 10) {
+            newValue = 0;
+          } else if (ui.value < 30) {
+            newValue = 20;
+          } else if (ui.value < 50) {
+            newValue = 40;
+          } else if (ui.value < 70) {
+            newValue = 60;
+          } else if (ui.value < 90) {
+            newValue = 80;
+          } else {
+            newValue = 100;
+          }
+          $(event.target).slider("value", newValue);
+        }
+      });
+    }
+  }
+</script>
+EOF;
 
 $page->body = <<<CONTENT
+<h1>Quicktest</h1>
 <p>
   Deze test bestaat uit 25 stellingen/vragen.
   <br />
-  Kies steeds in welke mate de uitspraak op U van toepassing is.
+  Kies steeds in welke mate de uitspraak op u van toepassing is.
 </p>
+<noscript>
+  <p>Voor het invullen van de tests moet <a href="http://enable-javascript.com/nl/" target="_blank">Javascript ingeschakeld zijn in uw browser</a>.</p>
+</noscript>
 CONTENT;
 
 // Elke vraag heeft een id. Deze hoeft niet per se gelijk te zijn aan de volgorde van de array. Dit zorgt ervoor dat er in de CMS vragen bij en af kunnen.
@@ -25,14 +68,14 @@ $vragen = array(
 	7 => 'Ik ben vaker ziek dan vroeger',
   8 => 'Mijn houding t.o.v. mijn werk is \'Waar zou ik me druk over maken?\' Ik sta er wat onverschillig tegenover',
   9 => 'Ik raak vaker betrokken bij conflicten',
-  10 => 'Het werk lijdt onder \'hoe ik me voel\'.',
-	11 => 'Ik gebruik meer koffie, alcohol, sigaretten, drugs of kalmerende middelen om mij beter te voelen',
+  10 => 'Mijn werk lijdt onder \'hoe ik me voel\'.',
+	11 => 'Ik gebruik meer koffie, alcohol, sigaretten, drugs of kalmerende middelen dan normaal om mij beter te voelen',
   12 => 'Met andere mensen communiceren is een bron van spanning',
-  13 => 'Ik kan me niet concenteren op mijn werk zoals voorheen',
+  13 => 'Ik kan me niet concentreren op mijn werk zoals voorheen',
 	14 => 'Het werk verveelt me',
   15 => 'Ik werk hard, maar bereik weinig',
   16 => 'Ik voel me gefrusteerd op mijn werk',
-	17 => 'Ik zie ertegen op naar het werk te gaan',
+	17 => 'Ik zie ertegen op om naar het werk te gaan',
 	18 => 'Sociale activiteiten putten me uit',
   19 => 'Seks vraagt teveel energie',
   20 => 'In mijn vrije tijd kijk ik voornamelijk TV',
@@ -48,11 +91,11 @@ $page->body .= "<form action=\"$php_self\" method=\"POST\">";
 foreach ($vragen as $id => $vraag) {
 	$page->body .= "<p>$vraag</p>";
   $page->body .= <<<EOF
-    <input type="range" min="0" max="5" step="1" value="2.5" />
+    <div id="slider$id" class="slider_handle"></div>
 EOF;
-
 }
+
+$page->body .= '<input type="submit" value="Versturen" class="button" />';
 $page->body .= '</form>';
-$page->body .= '<input type="submit" value="Versturen" />';
 
 $page->create();
