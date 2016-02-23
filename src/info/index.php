9b96486e100752f86c92f8cce598842c5060cabf
Nick Belzer <nickbelzer@gmail.com>
2016-02-23 13:30:14 +0100

    Adjusted path_to_root value since no longer needed

--- src/info/index.php
+++ src/info/index.php
@@ -6,8 +6,7 @@
 include '../resources/includes/PageCreator.php';
 $page = new PageCreator();
 $page->path_to_root = '../';
-$page->head = '<link rel="stylesheet" href="'.$page->path_to_root
-  .'resources/css/specific/information.css" type="text/css">';
+$page->head = '<link rel="stylesheet" href="resources/css/specific/information.css" type="text/css">';
 $page->title = "Informatie";
 $page->body = <<<CONTENT
 
