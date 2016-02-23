a0f016b0c9c312a25cfe5e04ed7e4941ab7cae2d
Nick Belzer <nickbelzer@gmail.com>
2016-02-23 13:28:30 +0100

    Started working on info page

--- src/info/index.php
+++ src/info/index.php
@@ -11,13 +11,48 @@ $page->head = '<link rel="stylesheet" href="'.$page->path_to_root
 $page->title = "Informatie";
 $page->body = <<<CONTENT
 
-<div class='row'>
-  <div class='medium-9 columns'>
-    Tekst voor informatie pagina
+<div class="content">
+
+  <div class="row">
+    <div class="medium-10 medium-centered columns">
+
+      <div class="medium-2 columns left">
+        <div class="menu">
+          <ul>
+            <a href="#"><li>Index</li></a>
+            <a href="#"><li>Ambitie</li></a>
+            <a href="#"><li>Methoden</li></a>
+            <a href="#"><li>Serge Janssen</li></a>
+          </ul>
+        </div>
+      </div>
+
+      <div class="medium-10 columns right">
+        <div class="text">
+          <h5>Wie zijn we?</h1>
+
+          <p>
+            Steunpunt Stress Burnout Nederland is een initiatief van Ria in de Weij en Serge Janssen en opgericht in 2011.
+            <br />
+            Samen hebben we jarenlange ervaring in het trainen en werken met mensen opgebouwd.
+            <br />
+            Ria heeft met name haar ervaring in de reïntegratiesector en Serge staat als docent al ruim 15 jaar voor de klas.
+          </p>
+
+          <p>
+            We zijn beiden registercounsellor bij de ABvC. Dit is de Algemene Beroepsvereniging voor Counselling. Naast een gedegen opleiding met als specialisatie stress en burnoutproblematiek, dienen we ons ook te houden aan de ethische beroepscode. Dit vraagt een jaarlijkse bijscholing en aantal uren intervisie, zodat de kwaliteit van onze  dienstverlening up-to-date blijft.
+          </p>
+
+          <p>
+            Ook van onze samenwerkingspartners in de provincies verwachten we een zelfde betrokkenheid als specialisatie, zodat uw stressvraag in goede handen is.
+          </p>
+        </div>
+      </div>
+
+
+    </div>
   </div>
-  <div class='medium-3 columns'>
-    Possible menu
-  </div>
+
 </div>
 
 CONTENT;
