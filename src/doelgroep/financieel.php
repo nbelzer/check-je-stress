3f854264fe7cc91c737babacd0336d60636f385b
Coolpower <martijnvansluijs@gmail.com>
2016-02-24 11:33:29 +0100

    Add initial doelgroep page

---
+++ src/doelgroep/financieel.php
@@ -0,0 +1,21 @@
+<h1>Financiële gevolgen voor bedrijven</h1>
+
+<p>
+  Ziekteverzuim, arbeidsongeschiktheid en mogelijke instroom in de WGA zijn mogelijke gevolgen van een burnout. Natuurlijk is dit in het ergste geval zo. Indien je een <b>goed ziekteverzuimbeleid</b<, een goed <b>preventieplan</b> hebt en actief bezig bent met het voorkomen van stress en burnout bij je personeel zal het zo een vaart niet lopen.
+</p>
+
+<p>
+  Toch kan het misgaan en raken er medewerkers burnout als gevolg van het werk. Het bedrijf kan <b>aansprakelijk gesteld</b> worden door letstelschadejuristen en er zijn in het verleden sommen door de rechter opgelegd, die variëren van €80.000,- tot zelfs € 237.000,-. 
+  <br />
+  Lees daarover <a href="http://vorige.nrc.nl/binnenland/article1820036.ece/www.stamos.nl">hier</a> meer. Verdere gedetailleerde informatie of heb je vragen of mogelijke <b>juridische gevolgen</b> kun je contact opnemen met letselverhalen.nl
+</p>
+
+<p>
+	Bovendien heeft de instroom van medewerkers in de <b>WGA</b> ook <b>verstrekkende gevolgen</b>. Een simpele rekensom:
+  <br />
+  Een bedrijf heeft een loonsom van € 10.000.000,- en heeft een parttimer die €10.000 verdiend, die in de WGA stroomt. Dan kost deze parttimer het bedrijf ongeveer € 20.000,- aan premie gedurende 10 jaar lang, dus <b>€ 200.000,-</b> die voorkomen had kunnen worden door een goed en actief preventiebeleid en <b>op tijd ingrijpen</b>. Je kan zelf uitrekenen hoeveel het kost als je meer medewerkers in de WGA hebt met hogere uitkeringen en/of een hogere loonsom.
+</p>
+
+<p>
+	Verdere gedetailleerde informatie en vragen over juistheid van premies inzake WGA en IVA kunt u het beste contact opnemen met VdW Subsidiedesk. (<a href="http://www.vdw-subsidiedesk.nl/">www.vdw-subsidiedesk.nl</a> of <a href="mailto:info@vdw-subsidiedesk.nl">info@vdw-subsidiedesk.nl</a>)
+</p>
--- src/doelgroep/index.php
+++ src/doelgroep/index.php
@@ -1,16 +1,194 @@
-<!--- Voor wie-->
-<!--- Links-->
-<!--- Steunpunten bij jou in de buurt-->
+<!--- Doelgroep-->
 
 <?php
 include '../resources/includes/PageCreator.php';
 $page = new PageCreator();
 $page->path_to_root = '../';
-
+$page->head = '<link rel="stylesheet" href="resources/css/specific/information.css" type="text/css">';
 $page->title = "Doelgroep";
 $page->body = <<<CONTENT
 
-Dit is de doelgroep pagina
+<div class="content">
+
+
+  <div class="row show-for-medium">
+    <div class="medium-10 medium-centered columns">
+      <div class="navmenu">
+        <ul class="vertical menu" data-magellan data-options="barOffset:40">
+          <li><a href="#first">Doelgroep</a></li>
+          <li><a href="#second">Particulier</a></li>
+          <li><a href="#third">Bedrijf</a><li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
+  <section class="text water" id="first">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+          <h5>Doelgroep</h1>
+
+          <p>
+            CheckJeStress is er voor zowel particulier als bedrijf. Beide partijen kunnen rekenen op onze inzet en ervaring.         
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <section class="text dark" id="second">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Particulier</h5>
+
+		  <p>
+			<b>Stress is onvermijdelijk en vroeg of laat kan je ermee te maken krijgen:</b> op je werk, in je sociale en privé-contacten, ja zelfs in je slaap. Steeds meer werkenden – zowel in het lagere als het hogere kader – klagen over stress of spanningen. Voor cijfers zie <a href="http://www.cbs.nl/nl-nl/menu/themas/arbeid-sociale-zekerheid/publicaties/artikelen/archief/2011/2011-3493-wm.htm">CBS.nl</a>
+		  </p>
+		  
+		  <p>
+			<b>Welke signalen waarschuwen voor een overbelasting aan stress?</b>
+			<br />
+			Signalen die op teveel stress duiden, variëren van een algeheel malaisegevoel tot fysieke pijn. Deze symptomen lopen zo uiteen dat men ze vaak niet herkent als tekenen van stress. Als men onder stress gebukt gaat, kan men zich angstig voelen, zich niet ontspannen of zich langdurig vervelen. Stress kan verder met slaapstoornissen gepaard gaan, met seksuele problemen en met verminderde arbeidsprestaties. Het vervelende van dit alles is, dat stress ook als heel persoonlijk ervaren wordt: waar de een niet opkijkt van 80 uur werken in de week, wordt de ander al misselijk bij het idee om een uur over te moeten werken.
+		  </p>
+
+		  <p>
+			<b>Stress en efficiëntie in het werk</b>
+			<br />
+			Uit ervaring blijkt dat afname van efficiëntie een brede uitstraling kan hebben op collega’s. Als een hele afdeling erdoor wordt beïnvloed, kan het voor een bedrijf beslist kostbaar worden. Dit bijverschijnsel van stress - burnout, de inefficiëntie, belast zowel de betreffende persoon als de hele omgeving. Het demoraliseert en is weer een oorzaak van verlies van eigenwaarde, dit gaat altijd gepaard met een burnout.
+		  </p>
+
+		  <p>
+			<b>Wat kan Steunpunt Stress Burnout Nederland voor jou betekenen?</b>
+			<ul>
+			  <li>We gaan onderzoeken hoe we de mentale belasting kunnen verminderen.
+			  <li>We helpen je het werk weer belonend te maken en persoonlijke betekenis te geven
+			  <li>We gaan je vermogen vergroten om een nieuwe burnout te voorkomen.
+			</ul>
+		  </p>
+		  
+		  <p>
+			<b>Voordelen van de begeleiding door Steunpunt Stress Burnout Nederland zijn:</b>
+			<ul>
+			  <li>Kracht en prestatievermogen worden behouden of snel herwonnen</li>
+			  <li>Binnen relatief korte tijd krijgen mensen met een burnout hun leven weer op de rails</li>
+			  <li>De behandeling is zeer effectief bij stress en het herstellen van de gevolgschade ervan</li>
+			</ul>
+		  </p>
+
+		  <p>
+			Herken je jezelf in bovenstaande symptomen en ben je benieuwd naar een stressvrij(er) leven? Neem dan contact met ons op door <a href="../contact/">hier te klikken</a>. Of vul test A in op de <a href="../tests/">testpagina</a>.
+		  </p>
+
+		  <p>
+			Je kunt zelf al eenvoudig beginnen om te zorgen voor een goed slaappatroon, maar ook regelmatig en gezond te eten. Neem tijd voor ontspanning door bijvoorbeeld te wandelen, zwemmen of te sporten. Je ziet dat dit al eenvoudige tips zijn om actief iets tegen stress te doen. Zit je echter dieper in de put en wil je een nieuw begin maken? Aarzel niet en neem contact op!
+		  </p>
+
+		  <p>
+			Verzekeringsmaatschappijen erkennen in toenemende mate de waarde van counselling en vergoeden (soms gedeeltelijk) de sessies, waaronder:
+			<br />
+			<i>Aegon, Anderzorg, CZ, Delta Lloyd, IAK, IZA, Lancyr, Menzis, Ohra, Trias, Univé</i> en <i>VGZ</i>. 
+			<a href="http://www.abvc.nl/CMS/Docs/Verz/Vergoedingen%20zorgverzekeraars%202013%20ABvC.pdf">Klik hier</a> voor een overzicht van de verzekeringsmaatschappijen en bijbehorende vergoedingen.
+		  </p> 
+
+		  <p>
+			De consulten/sessies van een individueel traject (geen groepstrainingen, lezingen en/of workshops) vallen onder "Alternatieve Geneeswijzen" in het aanvullende verzekeringspakket. Raadpleeg de eigen polis of neem contact op met de ziektekostenverzekeraar, deze kan je vertellen tot welk verzekerd bedrag onze declaraties vergoed worden.
+		  </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text water" id="third">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Bedrijf</h5>
+
+          <p>
+			<b>Elke werknemer telt! Verzuim als gevolg van stress en stressgerelateerde klachten (RSI!) kost de maatschappij miljoenen euro's per jaar!</b>
+		  </p>
+
+		  <p>
+			TNO berekende dat in 2006 verzuim als gevolg van stress en stressgerelateerde klachten € 6,1 miljard euro kostte. Daarin is het zogenaamde "stil verzuim" niet in meegenomen - mensen komen wel opdagen op het werk, maar zijn te moe of te uitgeblust om goed werk te leveren. 
+		  </p>
+		  
+		  <p>
+			Over duurzaam ondernemen spreken de media tegenwoordig vrij regelmatig. Maar hoe duurzaam ben je met je medewerkers? Grote kans dat er medewerkers zijn die het risico lopen om burn-out te raken. Sta je daar wel eens bij stil? 
+		  </p>
+
+		  <p>
+			Heb je dan al eens uitgerekend <b>wat die werknemer je dan kost</b>, mocht deze als gevolg van burn-out (verminderd) arbeidsongeschikt wordt?
+			<br />
+			<a href="../financieel/">Klik hier</a> voor een eenvoudig rekenvoorbeeld.
+		  </p>
+
+		  <p>
+			<b>Steunpunt Stress Burnout Nederland kan voor jouw bedrijf een beleidsplan schrijven ter preventie van burn-out, dit geheel toegesneden op jouw bedrijf, ongeacht grootte, branche of locatie. De arbeidsinspectie kan naar een preventiebeleid vragen!</b>
+		  </p>
+		  
+		  <p>
+			Dit betekent voor jouw bedrijf:*
+			<ul>
+			  <li>Uitvoeren van een test en intake per werknemer</li>
+			  <li>Doorlichten van de gehele organisatie op het gebied van werkdruk</li>
+			  <li>Individuele begeleiding van werknemers</li>
+			  <li>Opzetten van een op maat gemaakt beleidsplan ter preventie van stress en burnout</li>
+			  <li>Trainen van leidinggevenden om stress/burnout te herkennen bij medewerkers</li>
+			  <li>Voorlichting voor de medewerkers in de vorm van een lezing of workshop</li>
+			</ul>
+		  </p>
+		  
+		  <p>
+			<b>Samengevat enkele voordelen als werkgever op een rij:</b>
+			<ul>
+			  <li>Met een lage investering bereik je een hoog rendement</li>
+			  <li>Je voorkomt een lang ziekteverzuim van de werknemer</li>
+			  <li>Je geeft een gedeelte van het protocol voor preventie ziekteverzuim uit handen, zodat er meer tijd overblijft voor je bedrijfsvoering</li>
+			  <li>De stressvermindering bij de werknemer zorgt voor een betere werksfeer en daardoor voor een hogere productiviteit</li>
+			  <li>Je toont uw betrokkenheid aan de werknemer</li>
+			  <li>Je kiest voor maatwerk binnen een bewezen effectief concept</li>
+			  <li>Structurele aanpak stress en burnout</li>
+			  <li>Structurele aanpak van preventie ziekteverzuim</li>
+			  <li>Je voldoet aan wettelijke normen voor preventie ziekteverzuim</li>
+			</ul>
+		  </p>
+		  
+		  <p>
+			<b>Concreet houdt dit in:</b>
+			<br />
+			Persoonlijk leed voor de werknemer en een lang ziekteverzuim, hoge kosten voor de werkgever (loondoorbetaling, WGA, vervangingskosten enz.) en een lagere productie voor het bedrijf. Om nog maar niet te spreken van een slechte PR op feestjes en verjaardagen.
+		  </p>
+		  
+		  <p>
+			Bij een burnout is de werknemer langere tijd, variërend van 6 maanden tot soms 2 jaar, niet in staat zijn werk te verrichten. Bovendien heb je als werkgever de verplichting om goed werkgeversschap te leveren. Mogelijk kun je als bedrijf/organisatie aansprakelijk gesteld worden voor arbeidsongeschikt van een werknemer als gevolg van een burnout. Lees hier meer over de mogelijke gevolgen.
+		  </p>
+
+		  <p>
+			Ziekteverzuim, arbeidsongeschiktheid en mogelijk instroom in de WGA van je personeel heeft financiële gevolgen. Lees <a href="financieel/">hier</a> daarover meer.
+		  </p>
+
+		  <p>
+			<a href="../contact/">Klik hier</a> voor een vrijblijvende afspraak om de mogelijkheden voor je op een rijtje te zetten.
+		  </p>
+
+		  <p>
+			* al naar gelang de vraag van de organisatie
+		  </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+ 
+</div>
 
 CONTENT;
 $page->create();
+
