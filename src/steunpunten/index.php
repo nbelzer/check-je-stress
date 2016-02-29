f48e9f7b6627abeb1cf4e7f9e7b6093ae02367e0
Coolpower <martijnvansluijs@gmail.com>
2016-02-29 17:08:42 +0100

    Add initial steunpunten page

---
+++ src/steunpunten/index.php
@@ -0,0 +1,422 @@
+<!--- Steunpunten-->
+
+<?php
+include '../resources/includes/PageCreator.php';
+$page = new PageCreator();
+$page->path_to_root = '../';
+$page->head = '<link rel="stylesheet" href="resources/css/specific/information.css" type="text/css">';
+$page->title = "Steunpunten";
+$page->body = <<<CONTENT
+
+<div class="content">
+
+
+  <div class="row show-for-medium">
+    <div class="medium-10 medium-centered columns">
+      <div class="navmenu">
+        <ul class="vertical menu" data-magellan data-options="barOffset:40">
+          <li><a href="#first">Overzicht</a></li>
+          <li><a href="#second">Zuid-Holland</a></li>
+          <li><a href="#third">Noord-Holland</a><li>
+		  <li><a href="#fourth">Noord-Brabant</a><li>
+		  <li><a href="#fifth">Gelderland</a><li>
+		  <li><a href="#sixth">Utrecht</a><li>
+		  <li><a href="#seventh">Overijssel</a><li>
+		  <li><a href="#eighth">Limburg</a><li>
+		  <li><a href="#ninth">Friesland</a><li>
+		  <li><a href="#tenth">Groningen</a><li>
+		  <li><a href="#eleventh">Drenthe</a><li>
+		  <li><a href="#twelfth">Flevoland</a><li>
+		  <li><a href="#thirteenth">Zeeland</a><li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <section class="text water" id="first">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Overzicht</h5>
+
+		  <p>
+			*Hier komt de clickable map te staan*
+		  </p>
+		  
+		  <p>
+			Kies hierboven op de kaart de provincie naar keuze.
+		  </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text dark" id="second">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Zuid-Holland</h5>
+		  
+		  <p>
+			Alphen aan den Rijn<br>
+			<i>Counselling Alphen aan den Rijn</i><br>
+			<a href="http://www.counsellingalphenaandenrijn.nl">www.counsellingalphenaandenrijn.nl</a>
+		  </p>
+		  
+		  <p>
+			Leisden<br>
+			<i>Leni Minderhoud Counselling & Coaching</i><br>
+			<a href="http://www.leniminderhoud.nl">www.leniminderhoud.nl</a>
+		  </p>
+		  
+		  <p>
+			Honselersdijk<br>
+			<i>Molius Relaties</i><br>
+			<a href="http://www.molius.nl">www.molius.nl</a>
+		  </p>
+		    
+        </div>
+      </div>
+    </div>
+  </section>   
+  
+  <section class="text water" id="third">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Noord-Holland</h5>
+		  
+		  <p>
+			Amsterdam<br>
+			<i>De beweging / Hartfysiotherapie</i><br>
+			<a href="http://www.debewegingadvies.nl">www.debewegingadvies.nl</a>
+		  </p>
+		  
+		  <p>
+			Amsterdam<br>
+			<i>Tanya de Wit Loopbaanbegeleiding</i><br>
+			<a href="http://www.tanyadewit.nl">www.tanyadewit.nl</a>
+		  </p>
+		  
+		  <p>
+			Amsterdam<br>
+			<i>Coaching en Counseling Amsterdam</i><br>
+			<a href="http://www.coachingencounselingamsterdam.nl">www.coachingencounselingamsterdam.nl</a>
+		  </p>
+		  
+		  <p>
+			Kortenhoef<br>
+			<i>Aaratio</i><br>
+			<a href="http://www.aaratio.nl">www.aaratio.nl</a>
+		  </p>
+		  
+		  <p>
+			Purmerend<br>
+			<i>Well-linked</i><br>
+			<a href="http://www.well-linked.nl">www.well-linked.nl</a>
+		  </p>
+		  
+		  <p>
+			Krommenie<br>
+			<i>Emovisie (o.a. artiesten als specialiteit)</i><br>
+			<a href="http://www.emovisie.nl">www.emovisie.nl</a>
+		  </p>
+		  
+		  <p>
+			Medemblik<br>
+			<i>Primeres Counseling, Coaching en Training</i><br>
+			<a href="http://www.primeres.nl">www.primeres.nl</a>
+		  </p>
+		  
+		  <p>
+			Aalsmeer<br>
+			<i>Praktijk voor Counseling en Coaching</i><br>
+			<a href="http://www.saskiadriezen.nl">www.saskiadriezen.nl</a>
+		  </p>
+		  
+        </div>
+      </div>
+    </div>
+  </section>    
+
+  <section class="text dark" id="fourth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Noord-Brabant</h5>
+		  
+		  <p>
+			Kruisland<br>
+			<i>Counsellingpraktijk "INBUSS"</i><br>
+			<a href="http://www.inbuss.nl">www.inbuss.nl</a>
+		  </p>
+		  
+		  <p>
+			Sint Anthonis<br>
+			<i>Bewust Helen</i><br>
+			<a href="http://www.bewust-helen.nl">www.bewust-helen.nl</a>
+		  </p>
+		  
+		  <p>
+			Helvoirt<br>
+			<i>Stresskundig</i><br>
+			<a href="http://.stresskundig.nl">www.stresskundig.nl</a>
+		  </p>
+		  
+		  <p>
+			Tilburg<br>
+			<i>Mesters Coaching & Counselling</i><br>
+			<a href="http://www.mesters.nl">www.mesters.nl</a>
+		  </p>
+		  
+		  <p>
+			Sterksel<br>
+			<i>www.lifechoices.nl</i><br>
+			<a href="http://www.lifechoices.nl">www.lifechoices.nl</a>
+		  </p>
+		  
+        </div>
+      </div>
+    </div>
+  </section>   
+  
+  <section class="text water" id="fifth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Gelderland</h5>
+		  
+		  <p>
+			Arnhem<br>
+			<i>Batubulan Counseling</i><br>
+			<a href="http://www.batubulancounseling.nl">www.batubulancounseling.nl</a>
+		  </p>
+		  
+		  <p>
+			Nijmegen<br>
+			<i>Stil Counseling en Coaching</i><br>
+			<a href="http://www.stilcounseling.nl">www.stilcounseling.nl</a>
+		  </p>
+		  
+		  <p>
+			Lent<br>
+			<i>Ammerlaan Counseling</i><br>
+			<a href="http://www.ammerlaancounseling.nl">www.ammerlaancounseling.nl</a>
+		  </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text dark" id="sixth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Utrecht</h5>
+		  
+		  <p>
+			Leusden<br>
+			<i>Carolien Broeke</i><br>
+			<a href="http://www.carolienbroeke.nl">www.carolienbroeke.nl</a>
+		  </p>
+		  
+		  <p>
+			Amersfoort<br>
+			<i>Cirkel der Seizoenen</i><br>
+			<a href="http://www.cirkelderseizoenen.nl">www.cirkelderseizoenen.nl</a>
+		  </p>
+		  
+		  <p>
+			Houten<br>
+			<i>InEssence</i><br>
+			<a href="http://www.inessence.org">www.inessence.org</a>
+		  </p>
+		  
+		  <p>
+			Bosch en Duin<br>
+			<i>Cum Cura (kinderen 8 t/m 23 jaar)</i><br>
+			<a href="http://www.cumcura.nl">www.cumcura.nl</a>
+		  </p>
+		  
+		  <p>
+			Utrecht<br>
+			<i>Karakter, kunst & Co</i><br>
+			<a href="http://www.karakterkunstenco.com">www.karakterkunstenco.com</a>
+		  </p>
+		  	  
+        </div>
+      </div>
+    </div>
+  </section>   
+  
+  <section class="text water" id="seventh">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Overijssel</h5>
+		  
+		  <p>
+			Hardenberg<br>
+			<i>Deditio Counseling en Coaching Bureau</i><br>
+			<a href="http://www.deditio.nl">www.deditio.nl</a>
+		  </p>
+		  
+		  <p>
+			Deventer<br>
+			<i>Chiasmo Consult</i><br>
+			<a href="http://www.chiasmo.nl">www.chiasmo.nl</a>
+		  </p>
+		  
+		  <p>
+			Enschede<br>
+			<i>PMB Counselling en Coaching</i><br>
+			<a href="http://www.pmbcounsellingencoaching.nl">www.pmbcounsellingencoaching.nl</a>
+		  </p>
+		  
+        </div>
+      </div>
+    </div>
+  </section> 
+  
+  <section class="text dark" id="eighth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Limburg</h5>
+		  
+		  <p>
+			Maastricht<br>
+			<i>Centrum voor lichaam en geest</i><br>
+			<a href="http://www.centrumvoorlichaamengeest.eu">www.centrumvoorlichaamengeest.eu</a>
+		  </p>
+		  
+		  <p>
+			Weert<br>
+			<i>Puur Life Coaching</i><br>
+			<a href="http://www.puur-lifecoaching.nl">www.puur-lifecoaching.nl</a>
+		  </p>
+		  
+		  <p>
+			Horn<br>
+			<i>Serenity Coaching</i><br>
+			<a href="http://www.serenitycoaching.nl">www.serenitycoaching.nl</a>
+		  </p>
+		  
+		  <p>
+			Buggenum<br>
+			<i>Unfold Counselling & Coaching</i><br>
+			<a href="http://www.unfold-counselling.nl">www.unfold-counselling.nl</a>
+		  </p>
+		  
+        </div>
+      </div>
+    </div>
+  </section>   
+  
+  <section class="text water" id="ninth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Friesland</h5>
+		  
+		  <p>
+			We hebben (nog) geen steunpunten in Drenthe. <a href="vacatures">Klik hier</a> als u zich als steunpunt voor deze provincie aan wilt melden.
+		  </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <section class="text dark" id="tenth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Groningen</h5>
+		  
+		  <p>
+			Siddeburen<br>
+			<i>Hilma Dekens Counseling- nlp- Coaching</i><br>
+			<a href="http://www.hilmadekens.nl">www.hilmadekens.nl</a>
+		  </p>
+		  
+        </div>
+      </div>
+    </div>
+  </section> 
+  
+  <section class="text water" id="eleventh">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Drenthe</h5>
+
+          <p>
+			We hebben (nog) geen steunpunten in Drenthe. <a href="vacatures">Klik hier</a> als u zich als steunpunt voor deze provincie aan wilt melden.
+		  </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text dark" id="twelfth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Flevoland</h5>
+
+		  <p>
+			Dronten<br>
+			<i>Uw Coach</i><br>
+			<a href="http://www.uwcoach.nu">www.uwcoach.nu</a>
+		  </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text water" id="thirteenth">
+    <div class="row">
+      <div class="medium-10 medium-centered columns">
+        <div class="medium-9 columns medium-offset-3">
+
+          <h5>Zeeland</h5>
+		  
+		  <p>
+			Terneuzen<br>
+			<i>BriljansS*</i><br>
+			<a href="http://www.briljanss.nl">www.briljanss.nl</a>
+		  </p>
+		  
+		  <p>
+			Veere<br>
+			<i>Vice Versa Veere</i><br>
+			<a href="http://www.viceversaveere.nl">www.viceversaveere.nl</a>
+		  </p>
+		  
+        </div>
+      </div>
+    </div>
+  </section>  
+ 
+</div>
+
+CONTENT;
+$page->create();
+
