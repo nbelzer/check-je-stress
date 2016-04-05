212623ba7bb749672b1363c82f8394c15f58ef18
Omniscimus <omniscimus@icloud.com>
2016-04-05 22:10:34 +0200

    Add personeelstest stub

---
+++ src/tests/personeelstest.php
@@ -0,0 +1,10 @@
+<?php
+
+$organisatie = isset($_GET['organisatie']);
+$code = isset($_GET['code']);
+
+if ($organisatie && $code) {
+  // Toon de personeelstest
+} else {
+  // Toon pagina met informatie over de personeelstest
+}
