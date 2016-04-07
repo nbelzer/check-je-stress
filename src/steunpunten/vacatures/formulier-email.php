ca3da2e1e1219b484ea150bf97916107c422e9bd
Coolpower <martijnvansluijs@gmail.com>
2016-04-07 10:45:58 +0200

    Add PHPMailer functionality to vacatures page

--- src/steunpunten/vacatures/formulier-email.php
+++ src/steunpunten/vacatures/formulier-email.php
@@ -17,6 +17,25 @@ $opleidingwerkervaring = $_POST['opleidingwerkervaring'];
 $beroepsvereniging = $_POST['beroepsvereniging'];
 $verdereinformatie = $_POST['verdereinformatie'];
 
+$to = "checkjestresstest@gmail.com";
+$headers = "From: $to \r\n";
+$headers .= "Reply-To: $email \r\n";
+$email_subject = "CheckJeStress: Sollicitatie $naam";
+$email_body = "Er is een nieuwe sollicitatie binnengekomen van de site CheckJeStress.nl:<br><br>
+	Naam: $naam<br>
+	Praktijknaam: $praktijknaam<br>
+	Eigen website: $eigenwebsite<br>
+	Email-adres: $email<br>
+	Adres: $adres<br>
+	Stad en provincie: $stadprovincie<br>
+	Opleiding en werkervaring: $opleidingwerkervaring<br>
+	Beroepsvereniging: $beroepsvereniging<br>
+	Verdere informatie: $verdereinformatie";
+
+include '../../resources/includes/PHPMailer/mail.php';
+$mail = new Mailer;
+$mail->sendMail([$to], $email_subject, $email_body, $email_body);
+
 $page->body = <<<CONTENT
 
 <div class="content">
@@ -28,15 +47,9 @@ $page->body = <<<CONTENT
           <h5>Contact</h1>
 
 		  <p>
-			Voor- en achternaam: $naam<br>
-			Praktijknaam: $praktijknaam<br>
-			Eigen websitenaam: $eigenwebsite<br>
-			E-mailadres: $email<br>
-			Adres + postcode: $adres<br>
-			Stad + provincie: $stadprovincie<br>
-			Opleiding + werkervaring: $opleidingwerkervaring<br>
-			Beroepsvereniging: $beroepsvereniging<br>
-			Verdere informatie: $verdereinformatie
+			Hartelijk bedankt voor het versturen van uw sollicitatie!<br>
+			Er zal zo spoedig mogelijk contact met u op worden genomen.<br>
+			<a href="">Klik hier</a> om terug te keren naar de homepage.
 		  </p>
 		  
         </div>
