cbd9c555f8a3a997161d8958e56636953289334f
Nick Belzer <nickbelzer@gmail.com>
2016-04-12 11:24:29 +0200

    Default layout to test index page

--- src/tests/index.php
+++ src/tests/index.php
@@ -8,54 +8,61 @@ $page->body = <<<CONTENT
 
 
 <div class="content">
+  <div class="menuSpacing"></div>
 
-  <section class="text water" id="first">
+  <div class="indexImage">
     <div class="row">
-      <div class="medium-10 medium-centered columns">
-        <div class="medium-9 columns medium-offset-3">
-          <h1>Burnout Tests</h1>
-
-          <ul>
-            <li>
-              <!-- Test A -->
-              Doe de <a href="tests/snelle-test">Snelle Test</a> als je snel wilt weten of je
-              risico loopt op een burnout.
-              <br>
-              Deze test duurt ongeveer 2 minuten.
-            </li>
-            <li>
-              <!-- Test C -->
-              Doe de <a href="tests/uitgebreide-test">Uitgebreide Test</a> als je wilt weten of
-              je verschijnselen hebt van een echte burnout.
-              <br>
-              Deze test duurt ongeveer 15 minuten.
-            </li>
-            <li>
-              <!-- Test B -->
-              De <a href="tests/risicoanalyse">Burnout Risico Analyse</a> is een test voor
-              managers, bestuurders en directeuren om te weten te komen hoeveel risico het
-              bedrijf loopt op burnouts van werknemers.
-              <br>
-              Deze test duurt ongeveer 5 minuten.
-            </li>
-          <!-- Voor deze test kunnen we misschien het beste een inlogsysteem maken waarbij
-               de werknemers de link naar de test krijgen opgestuurd, i.p.v. hem hier neer
-               te zetten.
-            <li>
-              <a href="testd/">Test D</a> is een deel van de burn-outrisicoanalyse voor bedrijven, ondernemingen en organisaties. (56 items duur ± 15 minuten)
-            </li>
-          -->
-          </ul>
-          <p>
-            Alle feedback verschijnt na het versturen op je scherm en je weet meteen het
-            resultaat. Uiteraard worden alle verstuurde gegevens vertrouwelijk door ons
-            behandeld.
-          </p>
-
+      <div class="medium-12 medium-centered columns">
+        <div class="backgroundImage" style="background-image: url('resources/img/frontpagecolourbeach.svg');">
         </div>
       </div>
     </div>
-  </section>
+  </div>
+
+
+  <div class="row water">
+    <div class="medium-10 medium-centered columns">
+      <h1>Burnout Tests</h1>
+
+      <ul>
+        <li>
+          <!-- Test A -->
+          Doe de <a href="tests/snelle-test">Snelle Test</a> als je snel wilt weten of je
+          risico loopt op een burnout.
+          <br>
+          Deze test duurt ongeveer 2 minuten.
+        </li>
+        <li>
+          <!-- Test C -->
+          Doe de <a href="tests/uitgebreide-test">Uitgebreide Test</a> als je wilt weten of
+          je verschijnselen hebt van een echte burnout.
+          <br>
+          Deze test duurt ongeveer 15 minuten.
+        </li>
+        <li>
+          <!-- Test B -->
+          De <a href="tests/risicoanalyse">Burnout Risico Analyse</a> is een test voor
+          managers, bestuurders en directeuren om te weten te komen hoeveel risico het
+          bedrijf loopt op burnouts van werknemers.
+          <br>
+          Deze test duurt ongeveer 5 minuten.
+        </li>
+      <!-- Voor deze test kunnen we misschien het beste een inlogsysteem maken waarbij
+           de werknemers de link naar de test krijgen opgestuurd, i.p.v. hem hier neer
+           te zetten.
+        <li>
+          <a href="testd/">Test D</a> is een deel van de burn-outrisicoanalyse voor bedrijven, ondernemingen en organisaties. (56 items duur ± 15 minuten)
+        </li>
+      -->
+      </ul>
+      <p>
+        Alle feedback verschijnt na het versturen op je scherm en je weet meteen het
+        resultaat. Uiteraard worden alle verstuurde gegevens vertrouwelijk door ons
+        behandeld.
+      </p>
+
+    </div>
+  </div>
 </div>
 
 CONTENT;
