bc2088b6e7afa172cb0a48a6af5d40d2f0814314
Omniscimus <omniscimus@icloud.com>
2016-04-12 11:25:30 +0200

    Add PHP e-mail address validation to contact form

--- src/contact/formulier-email.php
+++ src/contact/formulier-email.php
@@ -17,43 +17,52 @@ if (isset($_POST['aanvinkvelden'])) {
 	}
 }
 
-session_start();
-require_once '../resources/captcha/securimage.php';
-$securimage = new Securimage();
-if ($securimage->check($_POST['captcha_code']) == false) {
-	/* Wrong captcha */
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 	$message = <<<EOF
-		De code die u heeft ingevuld klopt niet.<br>
-		<a href='javascript:history.go(-1)'><button class="button">Probeer het nog eens.</button></a>
+		Vul alstublieft een geldig e-mailadres in.<br>
+		<a href='javascript:history.go(-1)'><button class="button">Terug</button></a>
 EOF;
 } else {
-	$to = "checkjestresstest@gmail.com";
-	$headers = "From: $to \r\n";
-	$headers .= "Reply-To: $email \r\n";
-	$email_subject = "CheckJeStress: Contactaanvraag $naam";
-	$email_body = <<<EOF
-		Er is een nieuwe aanvraag binnengekomen van de site CheckJeStress.nl:<br><br>
-		Persoonsinformatie:<br>
-		<ul>
-			$naam<br>
-			$email
-		</ul>
-		<br>
-		Deze persoon heeft interesse in de volgende zaken:
-		<ul>$aanvinkvelden</ul>
-		Deze persoon liet het volgende bericht achter:<br>
-		<ul>$vragenopmerkingenideeën</ul>;
+
+	session_start();
+	require_once '../resources/captcha/securimage.php';
+	$securimage = new Securimage();
+	if ($securimage->check($_POST['captcha_code']) == false) {
+		/* Wrong captcha */
+		$message = <<<EOF
+			De code die u heeft ingevuld klopt niet.<br>
+			<a href='javascript:history.go(-1)'><button class="button">Probeer het nog eens.</button></a>
+EOF;
+	} else {
+		$to = "checkjestresstest@gmail.com"; // TODO in config
+		$headers = "From: $to \r\n";
+		$headers .= "Reply-To: $email \r\n";
+		$email_subject = "CheckJeStress: Contactaanvraag $naam";
+		$email_body = <<<EOF
+			Er is een nieuwe aanvraag binnengekomen van de site CheckJeStress.nl:<br><br>
+			Persoonsinformatie:<br>
+			<ul>
+				$naam<br>
+				$email
+			</ul>
+			<br>
+			Deze persoon heeft interesse in de volgende zaken:
+			<ul>$aanvinkvelden</ul>
+			Deze persoon liet het volgende bericht achter:<br>
+			<ul>$vragenopmerkingenideeën</ul>;
 EOF;
 
-	include '../resources/includes/PHPMailer/mail.php';
-	$mail = new Mailer;
-	$mail->sendMail([$to], $email_subject, $email_body, $email_body);
+		include '../resources/includes/PHPMailer/mail.php';
+		$mail = new Mailer;
+		$mail->sendMail([$to], $email_subject, $email_body, $email_body);
 
-	$message = <<<EOF
-		Hartelijk bedankt voor het versturen van uw contactformulier!<br>
-		Er zal zo spoedig mogelijk contact met u op worden genomen.<br>
-		<a href="">Klik hier</a> om terug te keren naar de homepage.
+		$message = <<<EOF
+			Hartelijk bedankt voor het versturen van uw contactformulier!<br>
+			Er zal zo spoedig mogelijk contact met u op worden genomen.<br>
+			<a href="">Klik hier</a> om terug te keren naar de homepage.
 EOF;
+	}
+
 }
 
 $page->body = <<<CONTENT
