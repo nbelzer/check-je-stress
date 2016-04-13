4a4c68986505fc78ee93f5d0ec680575976d0085
Coolpower <martijnvansluijs@gmail.com>
2016-04-13 14:46:00 +0200

    Rewrite some text on the vacatures page

--- src/steunpunten/vacatures/index.php
+++ src/steunpunten/vacatures/index.php
@@ -29,7 +29,7 @@ $page->body = <<<CONTENT
 		  </p>
 
 		  <p>
-			Deel jij deze ambitie, wil jij je steentje hieraan bijdragen èn herken je jezelf in onderstaand profiel?
+			Deelt u deze ambitie, wilt u uw steentje hieraan bijdragen èn herkent u uzelf in onderstaand profiel?
 		  </p>
 			<ul>
 			  <li>HBO werk- en denkniveau</li>
@@ -40,7 +40,7 @@ $page->body = <<<CONTENT
 			</ul>
 
 		  <p>
-			Past het profiel en heb je nog steeds interesse? Vul dan onderstaand contactformulier in.
+			Past het profiel en heeft u nog steeds interesse? Vul dan onderstaand contactformulier in.
 		  </p>
 
 		  <p>
@@ -67,52 +67,52 @@ $page->body = <<<CONTENT
 			<table border="1">
 
 			  <tr>
-				<td><label for="1">Voor- en achternaam</label></td>
+				<td><label for="1">Voor- en achternaam:</label></td>
 				<td><input type="text" name="naam" id="1"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="2">Praktijknaam</label></td>
+				<td><label for="2">Praktijknaam:</label></td>
 				<td><input type="text" name="praktijknaam" id="2"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="3">Eigen websitenaam</label></td>
+				<td><label for="3">Eigen websitenaam:</label></td>
 				<td><input type="text" name="eigenwebsite" id="3"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="4">E-mailadres</label></td>
+				<td><label for="4">E-mailadres:</label></td>
 				<td><input type="text" name="email" id="4"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="5">Adres + postcode</label></td>
+				<td><label for="5">Adres + postcode:</label></td>
 				<td><input type="text" name="adres" id="5"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="6">Stad + provincie</label></td>
+				<td><label for="6">Stad + provincie:</label></td>
 				<td><input type="text" name="stadprovincie" id="6"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="7">Opleiding + werkervaring</label></td>
+				<td><label for="7">Opleiding + werkervaring:</label></td>
 				<td><input type="text" name="opleidingwerkervaring" id="7"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="8">Beroepsvereniging</label></td>
+				<td><label for="8">Beroepsvereniging:</label></td>
 				<td><input type="text" name="beroepsvereniging" id="8"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="9">Verdere informatie</label></td>
+				<td><label for="9">Verdere informatie:</label></td>
 				<td><input type="text" name="verdereinformatie" id="9"></td>
 			  </tr>
 
 			  <tr>
-				<td><label for="10">Captcha</label></td>
+				<td><label for="10">Captcha:</label></td>
 				<td>
 				  <img id="captcha" src="resources/captcha/securimage_show.php" alt="CAPTCHA Image" style="border:1px solid black;" />
 				  <a href="#" onclick="document.getElementById('captcha').src = 'resources/captcha/securimage_show.php?' + Math.random(); return false">[ Andere afbeelding ]</a><br><br>
