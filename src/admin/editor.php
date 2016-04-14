129ee505605c6598ede7cfe4a67b02c22ed8b85b
Omniscimus <omniscimus@icloud.com>
2016-04-14 10:31:21 +0200

    List each editable page separately in CMS (#4)

--- src/admin/editor.php
+++ src/admin/editor.php
@@ -2,75 +2,22 @@
 
 include '../resources/includes/PageCreator.php';
 $page_creator = new PageCreator;
+$page_creator->path_to_root = '../';
 
-/**
- * Geeft een string met HTML-code voor een entry in de navigatie.
- *
- * @param string $file het bestand waarvoor de entry moet worden gemaakt
- * @param boolean $is_directory of er een entry voor een map of voor een bestand moet worden gemaakt
- * @param int $indents het aantal indents dat deze entry moet hebben
- * @return string een element voor de navigatie
- */
-function createNavEntry($file, $is_directory, $indents) {
-
-  // Verander '../info/index.php' naar 'info/index.php'
-  $path_from_webroot = substr($file, 3);
-  // Verander 'info/index.php' naar 'index.php'
-  $file = substr($file, strrpos($file, DIRECTORY_SEPARATOR) + 1);
-
-  // Voeg indents toe in het geval dat de entry in een map zit
-  $padding = 20 * $indents;
-
-  // Files moeten linken naar de editor; mappen niet, die zijn er alleen voor het overzicht.
-  if ($is_directory) {
-    $to_return = "<li style=\"padding-left: {$padding}px; color: blue;\">$file</li>";
-  } else {
-    $to_return = "<li style=\"padding-left: {$padding}px; color: red;\"><a href=\"admin/editor.php?page=$path_from_webroot\">$file</a></li><br />";
-  }
-
-  return $to_return;
-}
-
-/**
- * Loopt door alle bestanden in een map en maakt er navigatie voor. De map 'admin' wordt niet meegerekend.
- *
- * @param string $main de base directory waarvoor navigatie gemaakt moet worden
- * @param string $output een string waaraan de output gekoppeld wordt. Handig bij recursie
- * @param int $indents het aantal indents dat de nav entries vanaf deze map moeten hebben
- * @return string bruikbare HTML-code voor in de 'nav' div
- */
-function readDirs($main, $output, $indents) {
-  // Eerst wordt de $main directory gelezen en worden alle gevonden bestanden / mappen in deze arrays gezet.
-  $dirs = [];
-  $files = [];
-
-  $dirHandle = opendir($main);
-  while ($file = readdir($dirHandle)) {
-    if (is_dir($main . $file)) {
-      if ($file != 'admin' && $file != '.' && $file != '..') {
-        array_push($dirs, $file);
-      }
-    } else if ($file != '.htaccess') {
-      array_push($files, $file);
-    }
-  }
-
-  // Als het een map is, roep deze method dan weer aan (recursief).
-  foreach ($dirs as $file) {
-    $output .= createNavEntry($main . $file, true, $indents);
-    $output = readDirs($main . $file . '/', $output, $indents + 1);
-  }
-  foreach ($files as $file) {
-    $output .= createNavEntry($main . $file, false, $indents);
-  }
-
-  return $output;
-}
-
-// Maak de inhoud van de navigatiebalk waar de administrator bestanden kan selecteren om ze aan te passen
-$file_index = readDirs('../', '', 0);
+/* Inhoud van de navigatiebalk waar de administrator bestanden kan selecteren om
+   de tekst aan te passen */
 $navigation = <<<EOF
-  $file_index
+  <ul>
+    <li><a href="admin/editor.php?page=index.php">Home</a></li>
+    <li><a href="admin/editor.php?page=doelgroep/index.php">Doelgroep</a></li>
+    <li><a href="admin/editor.php?page=doelgroep/bedrijf.php">Doelgroep -> Bedrijf</a></li>
+    <li><a href="admin/editor.php?page=doelgroep/particulier.php">Doelgroep -> Particulier</a></li>
+    <li><a href="admin/editor.php?page=info.php">Info</a></li>
+    <li><a href="admin/editor.php?page=steunpunten/index.php">Steunpunten</a></li>
+    <li><a href="admin/editor.php?page=steunpunten/vacatures.php">Steunpunten -> Vacatures</a></li>
+    <li><a href="admin/editor.php?page=contact.php">Contact</a></li>
+    <li><a href="admin/editor.php?page=colofon.php">Colofon</a></li>
+  </ul>
 EOF;
 
 // Maak de inhoud van de editor
@@ -103,7 +50,6 @@ EOF;
 }
 
 // Maak de editor.php pagina
-$page_creator->path_to_root = '../';
 $page_creator->title = "CheckJeStress - Editor";
 $page_creator->head = <<<EOF
   <script src="https://cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
