nbelzer/check-je-stress

commit 7c61ca43ac176feb1ec0acc04e25f3918faa902d

Clean up unnecessary folders

src/info.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 include '../resources/includes/PageCreator.php';
 $page = new PageCreator();
 $page->path_to_root = '../';

src/contact/index.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 include '../resources/includes/PageCreator.php';
 $page = new PageCreator();
 $page->path_to_root = '../';
@@ -8,7 +8,7 @@ $page->body = <<<CONTENT
 
 <div class="content">
   <div class="menuSpacing"></div>
-  
+
   <div class="indexImage">
     <div class="row">
       <div class="medium-12 medium-centered columns">
@@ -16,12 +16,12 @@ $page->body = <<<CONTENT
         </div>
       </div>
     </div>
-  </div> 
-  
+  </div>
+
 	<div class="row text water" id="first">
 	  <div class="medium-10 medium-centered columns">
         <div class="medium-12 columns">
-		
+
           <h5>Contact</h5>
 
           <p>
@@ -47,7 +47,7 @@ $page->body = <<<CONTENT
     <div class="row text dark" id="second">
       <div class="medium-10 medium-centered columns">
         <div class="medium-12 columns>
-		
+
           <h5>Contactformulier</h5>
 
 		  <form method="post" name="contactformulier" action="contact/formulier-email.php">
@@ -87,10 +87,10 @@ $page->body = <<<CONTENT
 				<td>
 				  <img id="captcha" src="resources/captcha/securimage_show.php" alt="CAPTCHA Image" style="border:1px solid black;" />
 				  <a href="#" onclick="document.getElementById('captcha').src = 'resources/captcha/securimage_show.php?' + Math.random(); return false">[ Andere afbeelding ]</a><br><br>
-				  <input type="text" name="captcha_code" maxlength="6" id="4">				
+				  <input type="text" name="captcha_code" maxlength="6" id="4">
 				</td>
 			  </tr>
-			  
+
 			</table>
 
 			<p>

commit 09e563390da355f59f1cf82a232fb31f520ce181

Fix path errors caused by 7c61ca43ac176feb1ec0acc04e25f3918faa902d

src/colofon.php
@@ -1,7 +1,6 @@
 <?php
-include '../resources/includes/PageCreator.php';
+include 'resources/includes/PageCreator.php';
 $page = new PageCreator();
-$page->path_to_root = '../';
 $page->head = '<link rel="stylesheet" href="resources/css/specific/standard.css" type="text/css">';
 $page->title = "Colofon";
 $page->body = <<<CONTENT
@@ -17,7 +16,7 @@ $page->body = <<<CONTENT
       </div>
     </div>
   </div>
-  
+
   <div class="row text water" id="first">
     <div class="medium-10 medium-centered columns">
 

src/info.php
@@ -1,7 +1,6 @@
 <?php
-include '../resources/includes/PageCreator.php';
+include 'resources/includes/PageCreator.php';
 $page = new PageCreator();
-$page->path_to_root = '../';
 $page->head = '<link rel="stylesheet" href="resources/css/specific/information.css" type="text/css">';
 $page->title = "Informatie";
 $page->body = <<<CONTENT

src/resources/includes/MenuCreator.php
@@ -48,7 +48,7 @@ class MenuCreator {
     $menu =
       (new MenuBuilder('Home', ''))
         ->build()
-      . (new MenuBuilder('Informatie', 'info/'))
+      . (new MenuBuilder('Informatie', 'info'))
         ->build()
       . (new MenuBuilder('Doelgroep', 'doelgroep/'))
         ->appendElement('Particulier', 'doelgroep/particulier/')

src/resources/includes/generic.php
@@ -64,7 +64,7 @@ if (!isset($includeMenu)) {
     </div>
     <footer class="footer" id="footer">
       <p>
-        <a href="colofon/">Colofon</a>
+        <a href="colofon">Colofon</a>
         <br>
         © Copyright CheckJeStress 2016
       </p>